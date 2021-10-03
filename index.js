d0dcacc5af8bb72abf6108435322f1bf1fe8952b
Meshv Patel <77038149+meshv-p@users.noreply.github.com>
2021-10-03 11:57:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,37 +4,43 @@
 //link = https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=3684e6479e9a4ac29fd01258fe97dc0e
 let api = 'e217c1e1f57e46289989fd685a5f108b';
 let source = 'bbc-news'
-let category = 'science';
 let pgno = 1;
 // let link = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&page=${pgno}&pagesize=10&apiKey=${api}`;
-let link = `https://saurav.tech/NewsAPI/top-headlines/category/${category}/in.json`
-console.log(link);
+// let link = `https://saurav.tech/NewsAPI/top-headlines/category/${category}/in.json`
+// console.log(link);
 let title, content;
 let spinner = document.getElementById('spinner');
 let accordionExample = document.getElementById('accordionExample');
-let notes = '';
-let notes2 = '';
+
 let spin = `<div class="spinner-border text-primary" role="status">
 <span class="visually-hidden">Loading...</span>
 </div>`;
-let number = 1;
+window.addEventListener('load', () => {
+    loaddata('general')
+});
 
-document.getElementById('science').addEventListener('click',()=>{
-    category = "science";
-})
+let load  ;
+let notes = '';
+function categoryitem(id) {
+    notes = "";
+    console.log(id);
+    loaddata(id);
+    laod = id;
+}
 
 
-function loaddata() {
-
+function loaddata(s) {
+    let number = 1;
+    let category = s;
 
     //start xhr
     let xhr = new XMLHttpRequest();
 
+    //    let  category = c;
+    link = `https://saurav.tech/NewsAPI/top-headlines/category/${category}/in.json`
+    console.log(link);
     xhr.open('GET', link, true);
 
-    xhr.onprogress = function () {
-        // console.log("on progress");
-    }
 
     xhr.onreadystatechange = function () {
         // console.log("ready state" + this.readyState);
@@ -57,32 +63,27 @@ function loaddata() {
             let loop = obj.articles;
             // console.log(obj.articles.);
             //    console.log(obj.articles);
-            loop.forEach(function (e ) {
-                
-
-               
-
-                let article = `<div class="accordion-item" >
-            <h2 class="accordion-header" id="heading${number}">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${number}" aria-expanded="true" aria-controls="collapseOne">
-              <b>Breaking News ${number }:&nbsp&nbsp </b> ${e['title']}
-              </button>
-            </h2>
-            <div id="collapse${number}" class="accordion-collapse collapse " aria-labelledby="heading${number}" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <strong>
-                ${e['content']} <a href="${e['url']}" target="_blank">Read more</a>
-                </strong>
-                
-                </div>
-            </div>
-          </div>`;
+            loop.forEach(function (e) {
+                let article = `
+                          <div class="col-auto mx-auto my-2">  
+                            <div class="card" style="width: 18rem;">
+                                <img src=${e.urlToImage?e.urlToImage:"https://s.w-x.co/in-earthshine%281%29.jpg"} class="card-img-top" alt="image">
+                                <div class="card-body">
+                                    <h5 class="card-title">${e['title']?e['title']: ""}</h5>
+                                    <p class="card-text"> ${e['content']?e['content'].slice(0,157) :""}...</p>
+                                    <a href="${e['url']}" target="_blank">Read More</a>
+                                    <small class="text-muted ms-5 ps-4">${e.publishedAt.slice(0,10)}</small>
+                                </div>
+                            </div>    
+                          </div>`
+                    ;
                 notes += article;
                 number++;
-                
+
             });
             // console.log(notes);
             accordionExample.innerHTML = notes;
+            console.log('done');
 
         }
         else {
@@ -92,84 +93,77 @@ function loaddata() {
     }
 
     xhr.send();
-    
+
 }
 
-loaddata();
-// let page = document.documentElement.scrollHeight;
-// let m = window.scrollY;
-// console.log(m);
 
-function loaddata1() {
-    pgno++;
-    // link = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&page=${pgno}&pagesize=10&apiKey=${api}`;
+function loadmore(s) {
+    let number = 1;
+    let notes2 = '';
+    let category = s;
 
-   //start xhr
-   let xhr = new XMLHttpRequest();
+    //start xhr
+    let xhr = new XMLHttpRequest();
 
-   xhr.open('GET', link, true);
+    //    let  category = c;
+    link = `https://saurav.tech/NewsAPI/top-headlines/category/${category}/in.json`
+    //     console.log(link);
+    xhr.open('GET', link, true);
 
-   xhr.onprogress = function () {
-       // console.log("on progress");
-   }
 
-   xhr.onreadystatechange = function () {
-       // console.log("ready state" + this.readyState);
 
-       if (this.readyState == 4) {
-        document.getElementById('endspinner').classList.remove('spinner-border');
+    xhr.onreadystatechange = function () {
+        // console.log("ready state" + this.readyState);
 
-       }
+        if (this.readyState == 4) {
+            spinner.style.display = "none";
+        }
 
-   }
+    }
 
-   xhr.onload = function () {
-       if (this.status === 200) {
-           let obj = JSON.parse(this.responseText);
-           console.log(obj);
-           // title = obj.articles[2].title;
-           // content = obj.articles[2].content;
+    xhr.onload = function () {
+        if (this.status === 200) {
+            let obj = JSON.parse(this.responseText);
+            console.log(obj);
+            // title = obj.articles[2].title;
+            // content = obj.articles[2].content;
 
-           //showing news
-           let accordionExample = document.getElementById('accordionExample');
-           let loop = obj.articles;
-           // console.log(obj.articles.);
-           //    console.log(obj.articles);
-           loop.forEach(function (e ) {
+            //showing news
+            let accordionExample = document.getElementById('accordionExample');
+            let loop = obj.articles;
+            // console.log(obj.articles.);
+            //    console.log(obj.articles);
+            loop.forEach(function (e) {
+                let article2 = `<div class="col-auto mx-auto my-2">  
+                <div class="card" style="width: 18rem;">
+                    <img src=${e.urlToImage?e.urlToImage:"https://s.w-x.co/in-earthshine%281%29.jpg"} class="card-img-top" alt="image">
+                    <div class="card-body">
+                        <h5 class="card-title">${e['title']?e['title']: ""}</h5>
+                        <p class="card-text"> ${e['content']?e['content'].slice(0,196) :""}...</p>
+                        <a href="${e['url']}" target="_blank">Read More</a>
+                    </div>
+                </div>    
+              </div>`;
+                notes2 += article2;
+                number++;
 
-               let article = `<div class="accordion-item">
-           <h2 class="accordion-header" id="heading${number}">
-             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${number}" aria-expanded="true" aria-controls="collapseOne">
-             <b>Breaking News ${number}:&nbsp&nbsp </b> ${e['title']}
-             </button>
-           </h2>
-           <div id="collapse${number}" class="accordion-collapse collapse " aria-labelledby="heading${number}" data-bs-parent="#accordionExample">
-             <div class="accordion-body">
-               <strong>
-               ${ e['content']}
-               <a href="${e['url']}" target="_blank">Read more</a>
-               </strong>
-               
-               </div>
-           </div>
-         </div>`;
-               notes2 += article;
-               number++;
-           });
-           // console.log(notes);
-          let d = notes.concat(notes2);
-          accordionExample.innerHTML = d;
+            });
+            // console.log(notes);
+            // notes.append(notes2);
+            accordionExample.innerHTML = notes2;
 
-       }
-       else {
-           //  document.getElementById('d').innerHTML = "Some error occured..";
-           console.log("Some error occured..");
-       }
-   }
+            console.log('done');
+        }
+        else {
+            //  document.getElementById('d').innerHTML = "Some error occured..";
+            console.log("Some error occured..");
+        }
+    }
 
-   xhr.send();
-   
+    xhr.send();
+
 }
+
 
 
 
@@ -177,7 +171,7 @@ window.addEventListener('scroll', () => {
     if (scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
         console.log('i am at bottom');
         document.getElementById('endspinner').classList.add('spinner-border');
-        console.log('spinner added');
-        loaddata();
+        console.log(load);
+        loadmore(load);
     }
 })
